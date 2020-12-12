247d2992e931ef58b916a5cbaea46330cf96e2cf
Geekcloud-es <appastragit@gmail.com>
2020-12-12 15:45:11 +0100

    modify tsc without watch

--- client/dist/out-tsc/src/app/app-routing.module.js
+++ client/dist/out-tsc/src/app/app-routing.module.js
@@ -1,7 +1,32 @@
 import { __decorate } from "tslib";
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-const routes = [];
+import { HomeComponent } from './home/home.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { SalesQuotationComponent } from './sales-quotation/sales-quotation.component';
+import { SalesComponent } from './sales/sales.component';
+const routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    { path: 'inventory',
+        component: InventoryComponent
+    },
+    {
+        path: 'sales',
+        component: SalesComponent,
+        children: [{
+                path: 'quotation',
+                component: SalesQuotationComponent
+            }]
+    },
+];
 let AppRoutingModule = class AppRoutingModule {
 };
 AppRoutingModule = __decorate([
--- client/dist/out-tsc/src/app/app.module.js
+++ client/dist/out-tsc/src/app/app.module.js
@@ -6,19 +6,36 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { HomeComponent } from './home/home.component';
+import { SalesComponent } from './sales/sales.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { SalesQuotationComponent } from './sales-quotation/sales-quotation.component';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryCache, HttpLink } from "@apollo/client/core";
 let AppModule = class AppModule {
+    constructor(apollo) {
+        apollo.create({
+            link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+            cache: new InMemoryCache()
+        });
+    }
 };
 AppModule = __decorate([
     NgModule({
         declarations: [
             AppComponent,
-            MenuComponent
+            MenuComponent,
+            HomeComponent,
+            SalesComponent,
+            InventoryComponent,
+            SalesQuotationComponent
         ],
         imports: [
             BrowserModule,
             AppRoutingModule,
             BrowserAnimationsModule,
-            MaterialModule
+            MaterialModule,
+            HttpClientModule,
         ],
         providers: [],
         bootstrap: [AppComponent]
